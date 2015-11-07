416fc2a5ab8432dc284c29dc4a15d9854c0f2332
mkbh <mkbh@63036859-31a0-4017-a504-2c249174f432>
2015-11-07 10:15:33 +0000

    Change newsTable to ArticleTable

---
+++ src/com/inmaa/admin/service/IArticleService.java
@@ -0,0 +1,17 @@
+package com.inmaa.admin.service;
+
+import java.util.List;
+
+import com.inmaa.admin.persistence.Article;
+
+public interface IArticleService {
+	    public void supprimer(Article entity);
+		
+		public void enregistrer(Article entity);
+		
+		public void mettre_a_jour(Article entity);
+		
+		public List<Article> lister();
+
+		public int maxSeqno();
+}
---
+++ src/com/inmaa/admin/service/IArticleServiceImpl.java
@@ -0,0 +1,72 @@
+package com.inmaa.admin.service;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.inmaa.admin.persistence.Article;
+
+@Service("ArticleService")
+@Transactional
+public class IArticleServiceImpl implements IArticleService{
+
+	@Autowired
+	SessionFactory sessionFactory;
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@Override
+	public void supprimer(Article entity) {
+		// TODO Auto-generated method stub
+		sessionFactory.getCurrentSession().delete(entity);
+	}
+
+	@Override
+	public void enregistrer(Article entity) {
+		// TODO Auto-generated method stub
+		sessionFactory.getCurrentSession().saveOrUpdate(entity);
+		
+	}
+
+	@Override
+	public void mettre_a_jour(Article entity) {
+		// TODO Auto-generated method stub
+		sessionFactory.getCurrentSession().update(entity);
+	}
+
+	@Override
+	public List<Article> lister() {
+		// TODO Auto-generated method stub
+		return sessionFactory.getCurrentSession().createQuery("from Article").list();
+	}
+	@Override
+	public int maxSeqno() {
+		try {
+
+			Session session=sessionFactory.getCurrentSession();
+			SQLQuery sqlQuery=session.createSQLQuery("SELECT COALESCE(max(seqno),0) FROM Article");
+			int seqno=  ((BigInteger) sqlQuery.uniqueResult()).intValue();
+			return seqno;
+
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+}
